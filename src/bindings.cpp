8e19387288c9e03864ee637406035203d1c8596e
AndyBarcia <andybarcia4@gmail.com>
2025-10-21 10:34:43 +0200

    Remove multi-class functions

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -22,21 +22,6 @@ torch::Tensor pairwise_sigmoid_cross_entropy_forward(
     const float scale = 1.0f
 );
 
-torch::Tensor mc_sigmoid_cross_entropy_forward(
-    const torch::Tensor& logits,
-    const torch::Tensor& targets,
-    const torch::Tensor& class_mapping,
-    const int num_masks
-);
-
-torch::Tensor mc_sigmoid_cross_entropy_backward(
-    const torch::Tensor& grad_out,
-    const torch::Tensor& logits,
-    const torch::Tensor& targets,
-    const torch::Tensor& class_mapping,
-    const int num_masks
-);
-
 std::vector<torch::Tensor> dice_loss_forward(
     const torch::Tensor& logits,
     const torch::Tensor& targets,
@@ -63,26 +48,6 @@ torch::Tensor pairwise_dice_loss_forward(
     const float smooth,
     int64_t background_index = -1,
     const float scale = 1.0f
-);
-
-std::vector<torch::Tensor> mc_dice_loss_forward(
-    const torch::Tensor& logits,
-    const torch::Tensor& targets,
-    const torch::Tensor& class_mapping,
-    const float smooth,
-    const int num_masks
-);
-
-torch::Tensor mc_dice_loss_backward(
-    const torch::Tensor& grad_out,
-    const torch::Tensor& logits,
-    const torch::Tensor& targets,
-    const torch::Tensor& class_mapping,
-    const torch::Tensor& total_intersection_sum,
-    const torch::Tensor& total_p_sum,
-    const torch::Tensor& total_t_sum,
-    const float smooth,
-    const int num_masks
 );
 
 torch::Tensor pairwise_mask_loss_forward(
@@ -123,13 +88,9 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("forward_sigmoid_ce_loss", &sigmoid_cross_entropy_forward, "Sigmoid Cross Entropy forward (CUDA)");
     m.def("backward_sigmoid_ce_loss", &sigmoid_cross_entropy_backward, "Sigmoid Cross Entropy backward (CUDA)");
     m.def("forward_pw_sigmoid_ce_loss", &pairwise_sigmoid_cross_entropy_forward, "Sigmoid Cross Entropy forward (CUDA)");
-    m.def("forward_mc_sigmoid_ce_loss", &mc_sigmoid_cross_entropy_forward, "Sigmoid Cross Entropy forward (CUDA)");
-    m.def("backward_mc_sigmoid_ce_loss", &mc_sigmoid_cross_entropy_backward, "Sigmoid Cross Entropy backward (CUDA)");
     m.def("forward_dice_loss", &dice_loss_forward, "Dice loss forward (CUDA)");
     m.def("backward_dice_loss", &dice_loss_backward, "Dice loss backward (CUDA)");
     m.def("forward_pw_dice_loss", &pairwise_dice_loss_forward, "Dice loss forward (CUDA)");
-    m.def("forward_mc_dice_loss", &mc_dice_loss_forward, "Dice loss forward (CUDA)");
-    m.def("backward_mc_dice_loss", &mc_dice_loss_backward, "Dice loss backward (CUDA)");
     m.def("pairwise_mask_loss_forward", &pairwise_mask_loss_forward, "Dice+Sigmoid loss forward (CUDA)");
     m.def("mask_matching", &mask_matching, "Mask matching using OR-Tools (CUDA)");
     m.def("mask_matching_backward", &mask_matching_backward, "Mask matching backward (CUDA)");
